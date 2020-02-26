8822194ea8fba17a400c5775f2f740c46adfa013
Marcus Scalet <marcus.scalet@hotmail.com>
2020-02-26 16:42:16 -0300

    criado testes de insert, update e remove

---
+++ src/test/java/com/marcusscalet/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -0,0 +1,63 @@
+package com.marcusscalet.ecommerce.iniciandocomjpa;
+
+import com.marcusscalet.ecommerce.EntityManagerTest;
+import com.marcusscalet.ecommerce.model.Produto;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class OperacoesComTransacaoTest extends EntityManagerTest {
+
+    @Test
+    public void atualizarObjeto() {
+        Produto produto = new Produto();
+
+        produto.setId(1);
+        produto.setNome("Kindle Paperwhite");
+        produto.setDescricao("Conheça o novo Kindle");
+        produto.setPreco(new BigDecimal(599));
+
+        entityManager.getTransaction().begin();
+        entityManager.merge(produto);
+        entityManager.getTransaction().commit();
+
+        //executar clear para que o find abaixo busque o objeto direto do banco ao invés do gerenciado pela memória
+        entityManager.clear();
+
+        Produto produtoVerificado = entityManager.find(Produto.class, produto.getId());
+        Assert.assertNotNull(produtoVerificado);
+        Assert.assertEquals("Kindle Paperwhite", produtoVerificado.getNome());
+    }
+
+    @Test
+    public void removerObjeto() {
+        Produto produto = entityManager.find(Produto.class, 3);
+
+        entityManager.getTransaction().begin();
+        entityManager.remove(produto);
+        entityManager.getTransaction().commit();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, 3);
+        Assert.assertNull(produtoVerificacao);
+    }
+
+    @Test
+    public void inserirPrimeiroObjeto() {
+        Produto produto = new Produto();
+
+        produto.setId(2);
+        produto.setNome("Câmera Canon");
+        produto.setDescricao("A melhor definição para suas fotos");
+        produto.setPreco(new BigDecimal(5000));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertNotNull(produto);
+    }
+}
