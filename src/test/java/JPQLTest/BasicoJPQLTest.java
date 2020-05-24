d3abbc125b9a29ee4f551d0757c83f9d71471a8a
Marcus Scalet <marcus.scalet@hotmail.com>
2020-05-24 10:43:54 -0300

    Testes com jqpl selecionando entidades como retorno

--- src/test/java/JPQLTest/BasicoJPQLTest.java
+++ src/test/java/JPQLTest/BasicoJPQLTest.java
@@ -1,11 +1,14 @@
 package JPQLTest;
 
 import com.marcusscalet.ecommerce.entitymanager.EntityManagerTest;
+import com.marcusscalet.ecommerce.model.Cliente;
 import com.marcusscalet.ecommerce.model.Pedido;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class BasicoJPQLTest extends EntityManagerTest {
 
@@ -16,5 +19,36 @@ public class BasicoJPQLTest extends EntityManagerTest {
 
         Pedido pedido = typedQuery.getSingleResult();
         Assert.assertNotNull(pedido);
+    }
+
+    @Test
+    public void mostrarDiferencaQueries(){
+        String jpql = "select p from Pedido p where p.id = 1";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        Pedido pedido1 = typedQuery.getSingleResult();
+        Assert.assertNotNull(pedido1);
+
+        Query query = entityManager.createQuery(jpql);
+        Pedido pedido2 = (Pedido) query.getSingleResult();
+        Assert.assertNotNull(pedido2);
+
+//        List<Pedido> lista = query.getResultList();
+//        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void selecionarUmAtributoParaRetorno(){
+        String jpql = "select p.nome from Produto p";
+
+        TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
+        List<String> lista = typedQuery.getResultList();
+        Assert.assertTrue(String.class.equals(lista.get(0).getClass()));
+
+        String jpqlCliente = "select p.cliente from Pedido p";
+        TypedQuery<Cliente> typedQueryCliente = entityManager.createQuery(jpqlCliente, Cliente.class);
+        List<Cliente> listaClientes = typedQueryCliente.getResultList();
+        Assert.assertTrue(Cliente.class.equals(listaClientes.get(0).getClass()));
+
     }
 }
