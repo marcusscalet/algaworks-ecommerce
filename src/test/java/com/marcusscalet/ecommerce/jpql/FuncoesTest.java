5fdd6ba3a65d8b83322a9b4fb9c6c76f3806a800
Marcus Scalet <marcus.scalet@hotmail.com>
2020-06-22 14:37:50 -0300

    testes com paginacao

---
+++ src/test/java/com/marcusscalet/ecommerce/jpql/FuncoesTest.java
@@ -0,0 +1,99 @@
+package com.marcusscalet.ecommerce.jpql;
+
+import com.marcusscalet.ecommerce.entitymanager.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class FuncoesTest extends EntityManagerTest {
+
+    @Test
+    public void aplicarFuncaoAgraegacao(){
+
+        String jpql = "select max(p.total) from Pedido p";
+
+        TypedQuery<Number>  typedQuery = entityManager.createQuery(jpql, Number.class);
+
+        List<Number> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(obj));
+    }
+
+    @Test
+    public void aplicarFuncaoNativa(){
+        String jpql = "select function('dayname', p.dataCriacao) from Pedido p" +
+                " where function('acima_media_faturamento', p.total) = 1";
+
+        TypedQuery<String>  typedQuery = entityManager.createQuery(jpql, String.class);
+
+        List<String> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(obj));
+    }
+
+    @Test
+    public void aplicarFuncaoColecao(){
+        String jpql = "select size (p.itens) from Pedido p where size(p.itens) > 1";
+
+        TypedQuery<Integer> typedQuery = entityManager.createQuery(jpql, Integer.class);
+
+        List<Integer> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(size -> System.out.println(size));
+    }
+
+    @Test
+    public void aplicarFuncaoNumero() {
+        String jpql = "select abs(p.total), mod(p.id, 2), sqrt(p.total) from Pedido p " +
+                " where abs(p.total) > 400";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
+    }
+
+    @Test
+    public void aplicarFuncaoData() {
+
+//        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
+//        String jpql = "select current_date, current_time, current_timestamp from Pedido p" +
+//                " where p.dataCriacao < current_date";
+
+//        String jpql = "select year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao)" +
+//                " from Pedido p";
+
+        String jpql = "select hour(p.dataCriacao), minute(p.dataCriacao), second(p.dataCriacao)" +
+                " from Pedido p";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
+    }
+
+    @Test
+    public void aplicarFuncaoString() {
+        // concat, length, locate, substring, lower, upper, trim
+
+        String jpql = "select c.nome, length(c.nome) from Categoria c " +
+                " where substring(c.nome, 1, 1) = 'N'";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " - " + arr[1]));
+    }
+}
